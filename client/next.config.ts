839bde7783c9773316780cbd0aad2322fda12452
Punya Chadha <punya.tkc@gmail.com>
2025-06-28 15:42:55 +0530

    s3 changes

--- client/next.config.ts
+++ client/next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.amazon.com",
+        port: "",
+        pathname: "/**",
+      }
     ]
   }
 };
