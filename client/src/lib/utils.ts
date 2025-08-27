1499a33851c0503404f1d5bb490f660e9e6f34df
Punya Chadha <punya.tkc@gmail.com>
2025-08-27 23:31:21 +0530

    removed the changes

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -20,31 +20,31 @@ export function formatPriceValue(value: number | null, isMin: boolean) {
   return isMin ? `$${value}+` : `<$${value}`;
 }
 
-// // eslint-disable-next-line @typescript-eslint/no-explicit-any
-// export function cleanParams(params: Record<string, any>): Record<string, any> {
-//   return Object.fromEntries(
-//     Object.entries(params).filter(
-//       (
-//         [_, value] // eslint-disable-line @typescript-eslint/no-unused-vars
-//       ) =>
-//         value !== undefined &&
-//         value !== "any" &&
-//         value !== "" &&
-//         (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
-//     )
-//   );
-// }
-
-export function cleanParams<T extends Record<string, unknown>>(params: T): Partial<T> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function cleanParams(params: Record<string, any>): Record<string, any> {
   return Object.fromEntries(
-    Object.entries(params).filter(([, value]) =>
-      value !== undefined &&
-      value !== "any" &&
-      value !== "" &&
-      (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
+    Object.entries(params).filter(
+      (
+        [_, value] // eslint-disable-line @typescript-eslint/no-unused-vars
+      ) =>
+        value !== undefined &&
+        value !== "any" &&
+        value !== "" &&
+        (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
     )
-  ) as Partial<T>;
+  );
 }
+
+// export function cleanParams<T extends Record<string, unknown>>(params: T): Partial<T> {
+//   return Object.fromEntries(
+//     Object.entries(params).filter(([, value]) =>
+//       value !== undefined &&
+//       value !== "any" &&
+//       value !== "" &&
+//       (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
+//     )
+//   ) as Partial<T>;
+// }
 
 
 type MutationMessages = {
